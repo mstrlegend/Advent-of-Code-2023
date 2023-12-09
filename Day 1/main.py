5071ff109794f6c581c62d78b70a1e49ace5504e
mstrlegend <92732442+mstrlegend@users.noreply.github.com>
2023-12-09 04:35:54 +0000

    ..

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,1 +1,13 @@
+#Challenge 1
 
+import re
+file = open("input.txt", "r")
+combined = []
+total = 0
+for line in file:
+    num1 = re.search("\d", line).group(0)
+    num2 = re.search(".*(\d)", line).group(1)
+    combined.append(int(num1 + num2))
+print(sum(combined))
+
+#Answer is correct, confirmed to be 55816
