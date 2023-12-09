bee6ac85754b78d0dafc997bf784e97e9e707fd1
mstrlegend <92732442+mstrlegend@users.noreply.github.com>
2023-12-09 11:45:08 +0000

    ..

--- Day 1/Challenge_1.py
+++ Day 1/Challenge_1.py
@@ -3,9 +3,8 @@
 import re
 file = open("input.txt", "r")
 combined = []
-total = 0
 for line in file:
-    num1 = re.search("\d", line).group(0)
+    num1 = re.search("(\d).*", line).group(1)
     num2 = re.search(".*(\d)", line).group(1)
     combined.append(int(num1 + num2))
 print(sum(combined))
--- Day 1/Challenge_2.py
+++ Day 1/Challenge_2.py
@@ -3,16 +3,30 @@
 import re
 file = open("input.txt", "r")
 combined = []
-words = ["one", "two", "three", "four","five", "six", "seven", "eight", "nine"]
-converted = ["1","2","3","4","5","6","7","8","9"]
-total = 0
 for line in file:
-    if words in line:
-        index = words.index(words)
-        line.replace(converted[index])
-    num1 = re.search("\d", line).group(0)
+    if "one" in line:
+        line = line.replace("one", "1")
+    if "two" in line:
+        line = line.replace("two", "2")
+    if "three" in line:
+        line = line.replace("three", "3")
+    if "four" in line:
+        line = line.replace("four", "4")
+    if "five" in line:
+        line = line.replace("five", "5")
+    if "six" in line:
+        line = line.replace("six", "6")
+    if "seven" in line:
+        line = line.replace("seven", "7")
+    if "eight" in line:
+        line = line.replace("eight", "8")
+    if "nine" in line:
+        line = line.replace("nine", "9")
+    print(line, end="")
+    num1 = re.search("(\d).*", line).group(1)
     num2 = re.search(".*(\d)", line).group(1)
     combined.append(int(num1 + num2))
+print(combined)
 print(sum(combined))
 
 #Answer is 
